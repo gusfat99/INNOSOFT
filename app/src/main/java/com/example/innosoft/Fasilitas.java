7e14cf57e3231ce3b5a5859141c626e183eb36bd
Bagus Fatwan Alfiat <fatwanbagus.99@gmail.com>
2020-07-21 20:04:50 +0700

    rycycleView fasilitas

---
+++ app/src/main/java/com/example/innosoft/Fasilitas.java
@@ -0,0 +1,29 @@
+package com.example.innosoft;
+
+public class Fasilitas {
+    private String title, description, imgView;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgView() {
+        return imgView;
+    }
+
+    public void setImgView(String imgView) {
+        this.imgView = imgView;
+    }
+}
