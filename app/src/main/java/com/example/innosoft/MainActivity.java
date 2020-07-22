794242d3f98b94000cf0da9c47cd14c398310b5a
RiskyAdit06 <riski.smkgalileo@gmail.com>
2020-07-22 12:16:24 +0700

    Spalash Screen Beres

--- app/src/main/java/com/example/innosoft/MainActivity.java
+++ app/src/main/java/com/example/innosoft/MainActivity.java
@@ -2,13 +2,15 @@ package com.example.innosoft;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
